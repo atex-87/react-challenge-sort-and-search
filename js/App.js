9a3358a256c1351a12fb78bfc754eaf05ef7cf78
yulia <yulia@daniel-spb.ru>
2017-08-03 10:54:08 +0300

    #

--- js/App.js
+++ js/App.js
@@ -1,40 +1,45 @@
 import React, { Component } from 'react';
 import Button from './components/Button';
+import UserList  from './components/UserList';
+ const load =  url => {
+  return new Promise((success, fail) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.addEventListener('load', () => {
+      request.status >= 200 && request.status < 400
+        ? success(request.responseText)
+        : fail(new Error(`Request Failed: ${request.statusText}`));
+    });
+
+    request.addEventListener('error', () => {
+      fail(new Error('Network Error'));
+    });
+
+    request.send();
+  });
+};
 
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: null,
       phrase: 'Нажми на кнопку!',
       count: 0
     };
+    this.loadData();
   }
-
+  loadData() {
+    load('/data.json').then(users => {
+      this.setState({
+        data: JSON.parse(users)
+      });
+  });
+}
   updateBtn() {
-    const phrases = [
-      'ЖМИ!', 'Не останавливайся!',
-      'У тебя хорошо получается!', 'Красавчик!',
-      'Вот это и есть React!', 'Продолжай!',
-      'Пока ты тут нажимаешь кнопку другие работают!',
-      'Всё хватит!', 'Ну и зачем ты нажал?',
-      'В следующий раз тут будет полезный совет',
-      'Чего ты ждешь от этой кнопки?',
-      'Если дойдёшь до тысячи, то сразу научищься реакту',
-      'ой, всё!', 'Ты нажал кнопку столько раз, что обязан на ней жениться',
-      'У нас было 2 npm-пакета с реактом, 75 зависимостей от сторонних библиотек, '
-      + '5 npm-скриптов и целое множество плагинов галпа всех сортов и расцветок, '
-      + 'а также redux, jquery, mocha, пачка плагинов для eslint и ингерация с firebase. '
-      + 'Не то что бы это был необходимый набор для фронтенда. Но если начал собирать '
-      + 'вебпаком, становится трудно остановиться. Единственное, что вызывало у меня '
-      + 'опасения - это jquery. Нет ничего более беспомощного, безответственного и испорченного, '
-      + 'чем рядовой верстальщик без jquery. Я знал, что рано или поздно мы перейдем и на эту дрянь.',
-      'coub про кота-джедая: http://coub.com/view/spxn',
-      'Дальнобойщики на дороге ярости: http://coub.com/view/6h0dy',
-      'Реакция коллег на ваш код: http://coub.com/view/5rjjw',
-      'Енот ворует еду: http://coub.com/view/xi3cio',
-      'Российский дизайн: http://coub.com/view/16adw5i0'
-    ];
+    
     this.setState({
       count: this.state.count + 1,
       phrase: phrases[parseInt(Math.random() * phrases.length)]
@@ -46,6 +51,7 @@ export default class App extends Component {
       <div className="container app">
         <Button count={this.state.count} update={this.updateBtn.bind(this)} />
         <p style={{marginTop: 2 + 'rem'}}>{this.state.phrase}</p>
+        <UserList data={this.state.data}/>
       </div>
     );
   }
